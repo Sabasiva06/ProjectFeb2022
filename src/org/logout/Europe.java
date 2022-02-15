cd885e155b23a3dca70cd6b95df80c6d5043d25e
Sabasiva06 <sabasiva06@gmail.com>
2022-02-15 17:17:06 +0530

    B

--- src/org/logout/Europe.java
+++ src/org/logout/Europe.java
@@ -5,6 +5,14 @@ public class Europe {
 	public void england() {
 		System.out.println("GMT");
 	}
-	
-	
+
+	public void australia() {
+		System.out.println("GMT+11.30");
+
+	}
+
+	public void russia() {
+		System.out.println("GMT+6.30");
+	}
+
 }
